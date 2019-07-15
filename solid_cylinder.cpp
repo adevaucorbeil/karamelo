a61c8b270394343b12c2be79fd2f23eae1821133
Alban de Vaucorbeil <alban.devaucorbeil@monash.edu>
2019-07-15 13:23:38 +1000

    Make a cylinder particle creation identical to that of a rectangle

--- solid_cylinder.cpp
+++ solid_cylinder.cpp
@@ -38,22 +38,30 @@ SolCylinder::SolCylinder(MPM *mpm, vector<string> args) : Solid(mpm, args)
   grid->init(solidlo, solidhi);
 
   // Calculate total number of particles np:
-  int nx, ny, nz;
-  nx = grid->nx - 3;
-  ny = grid->ny - 3;
-  if (domain->dimension == 3) nz = grid->nz - 3;
-  else nz = 1;
+  int np, nx, ny, nz;
+  double delta;
+  double hdelta;
+
+  delta = grid->cellsize;///((int) input->parsev(args[3]));
+
+  double Lx = solidhi[0]-solidlo[0];
+  double Ly = solidhi[1]-solidlo[1];
+
+  nx = ((int) Lx/delta);
+  ny = ((int) Ly/delta);
+
+  if (domain->dimension == 3) {
+    double Lz = solidhi[2]-solidlo[2];
+    nz = ((int) Lz/delta);
+   } else {
+    nz = 1;
+   }
+
   np = nx*ny*nz;
 
 
   // Create particles:
 
-  double delta;
-  double hdelta;
-
-  delta = grid->cellsize;
-
-  hdelta = 0.5*delta;
 
   cout << "delta = " << delta << endl;
 
@@ -77,8 +85,7 @@ SolCylinder::SolCylinder(MPM *mpm, vector<string> args) : Solid(mpm, args)
 	  x0[l][1] = x[l][1] = solidlo[1] + delta*(j+0.5);
 	  if (domain->dimension == 3) x0[l][2] = x[l][2] = solidlo[2] + delta*(k+0.5);
 	  else x0[l][2] = x[l][2] = 0;
-	  // Check if the particle is inside cylinder:
-	  if (domain->regions[iregion]->inside(x0[l][0], x0[l][1], x0[l][2])==1) l++;
+	  l++;
 	}
       }
     }
@@ -110,19 +117,67 @@ SolCylinder::SolCylinder(MPM *mpm, vector<string> args) : Solid(mpm, args)
 	    x0[l][1] = x[l][1] = solidlo[1] + delta*(j+0.5+intpoints[ip][1]);
 	    if (domain->dimension == 3) x0[l][2] = x[l][2] = solidlo[2] + delta*(k+0.5+intpoints[ip][2]);
 	    else x0[l][2] = x[l][2] = 0;
-	    // Check if the particle is inside cylinder:
-	    if (domain->regions[iregion]->inside(x0[l][0], x0[l][1], x0[l][2])==1) l++;
+	    l++;
 	  }
 	}
       }
-    }    
+    }
+  } else if ((int) input->parsev(args[3]) == 3) {
+    // Berstein elements:
+
+    np *= 27;
+    mass_ /= 27.0;
+    vol_ /= 27.0;
+    // Allocate the space in the vectors for np particles:
+    grow(np);
+
+    double a = 0.7746/2;
+
+    double intpoints[27][3] = {{-a, -a, -a},
+			       {-a, -a, 0},
+			       {-a, -a, a},
+			       {-a, 0, -a},
+			       {-a, 0, 0},
+			       {-a, 0, a},
+			       {-a, a, -a},
+			       {-a, a, 0},
+			       {-a, a, a},
+			       {0, -a, -a},
+			       {0, -a, 0},
+			       {0, -a, a},
+			       {0, 0, -a},
+			       {0, 0, 0},
+			       {0, 0, a},
+			       {0, a, -a},
+			       {0, a, 0},
+			       {0, a, a},
+			       {a, -a, -a},
+			       {a, -a, 0},
+			       {a, -a, a},
+			       {a, 0, -a},
+			       {a, 0, 0},
+			       {a, 0, a},
+			       {a, a, -a},
+			       {a, a, 0},
+			       {a, a, a}};
+
+    for (int i=0; i<nx; i++){
+      for (int j=0; j<ny; j++){
+	for (int k=0; k<nz; k++){
+	  for (int ip=0; ip<27; ip++) {
+	    x0[l][0] = x[l][0] = solidlo[0] + delta*(i+0.5+intpoints[ip][0]);
+	    x0[l][1] = x[l][1] = solidlo[1] + delta*(j+0.5+intpoints[ip][1]);
+	    if (domain->dimension == 3) x0[l][2] = x[l][2] = solidlo[2] + delta*(k+0.5+intpoints[ip][2]);
+	    else x0[l][2] = x[l][2] = 0;
+	    l++;
+	  }
+	}
+      }
+    }
   } else {
     cout << "Error: solid command 4th argument should be 1 or 2, but " << (int) input->parsev(args[3]) << "received.\n";
     exit(1);
   }
-
-  np = l; // Adjust np to account for the particles outside the domain
-  cout << "np=" << np << endl;
 
   for (int i=0; i<np;i++) {
     a[i].setZero();
