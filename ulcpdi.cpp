68c8eba5374192a9cd3651062594b875dcc5719a
Alban de Vaucorbeil <alban.devaucorbeil@monash.edu>
2019-09-20 17:14:34 +1000

    Using MUSL with ULCPDI without problem

--- ulcpdi.cpp
+++ ulcpdi.cpp
@@ -363,7 +363,7 @@ void ULCPDI::compute_rate_deformation_gradient()
 {
   for (int isolid=0; isolid<domain->solids.size(); isolid++) {
     if (method_type.compare("APIC") == 0) domain->solids[isolid]->compute_rate_deformation_gradient_UL_APIC();
-    else domain->solids[isolid]->compute_rate_deformation_gradient_UL_USL();
+    else domain->solids[isolid]->compute_rate_deformation_gradient_UL_MUSL();
     //domain->solids[isolid]->compute_deformation_gradient();
   }
 }
