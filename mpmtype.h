33d0a876ea33625c1cdef3b9cc7236561b262bb3
Alban de Vaucorbeil <alban.devaucorbeil@monash.edu>
2019-10-02 11:25:17 +1000

    Correct MPM type for bigint and tagint

--- mpmtype.h
+++ mpmtype.h
@@ -3,5 +3,5 @@
 typedef int64_t bigint;
 typedef int64_t tagint;
 
-#define MPI_MPM_TAGINT MPI_LL
-#define MPI_MPM_BIGINT MPI_LL
+#define MPI_MPM_TAGINT MPI_INT64_T
+#define MPI_MPM_BIGINT MPI_INT64_T
