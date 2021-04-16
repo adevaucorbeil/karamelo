4a830938d13aa1ce9c04cfcc96a2ba57dd144aeb
Alban de Vaucorbeil <alban.devaucorbeil@deakin.edu.au>
2021-04-16 13:36:18 +1000

    Add mask as a grid output

--- src/dump_grid_gz.cpp
+++ src/dump_grid_gz.cpp
@@ -126,12 +126,16 @@ void DumpGridGz::write() {
           dumpstream << g->mb[i][2] << " ";
         else if (v.compare("mass") == 0)
           dumpstream << g->mass[i] << " ";
+	else if (v.compare("mask")==0)
+	  dumpstream << g->mask[i] << " ";
         else if (v.compare("ntypex") == 0)
           dumpstream << g->ntype[i][0] << " ";
         else if (v.compare("ntypey") == 0)
           dumpstream << g->ntype[i][1] << " ";
         else if (v.compare("ntypez") == 0)
           dumpstream << g->ntype[i][2] << " ";
+        else if (v.compare("rigid") == 0)
+          dumpstream << g->rigid[i] << " ";
       }
       dumpstream << endl;
     }
