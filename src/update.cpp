23b04fce932060343e44669e318ea19796814cd9
Alban de Vaucorbeil <alban.devaucorbeil@deakin.edu.au>
2021-06-08 20:41:02 +1000

    Add option to get gradient-enhanced

--- src/update.cpp
+++ src/update.cpp
@@ -175,6 +175,17 @@ void Update::create_method(vector<string> args){
   }
   n++;
 
+  if (args.size() == n + 1) {
+    if (args[n].compare("gradient-enhanced") == 0) {
+      method->ge = true;
+    } else {
+      error->all(FLERR,
+                 "Illegal modify_method command: keyword " + args[n] +
+                     " unknown. Expected \"gradient-enhanced\" or nothing.\n");
+    }
+  }
+  n++;
+
   if (n < args.size()) {
     additional_args = vector<string>(args.begin() + n, args.end());
   } else {
