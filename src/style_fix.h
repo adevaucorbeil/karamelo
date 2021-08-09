b30ef7ba7ae512182c5f51ff23401638a6ca4052
Alban de Vaucorbeil <alban.devaucorbeil@deakin.edu.au>
2021-08-09 13:44:26 +1000

    Adding fix_temperature_particles

--- src/style_fix.h
+++ src/style_fix.h
@@ -13,5 +13,6 @@
 #include "fix_initial_velocity_particles.h"
 #include "fix_meld_tool.h"
 #include "fix_temperature_nodes.h"
+#include "fix_temperature_particles.h"
 #include "fix_velocity_nodes.h"
 #include "fix_velocity_particles.h"
