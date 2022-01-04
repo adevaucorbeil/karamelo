44c3f02ba31ff80c680120647fdccca67c2df8b6
Edward Buckland <edwardnbuckland@gmail.com>
2022-01-05 00:55:32 +1100

    Wilkinson shifting

--- src/mathematics/linear_algebra/eigendecompose.h
+++ src/mathematics/linear_algebra/eigendecompose.h
@@ -11,13 +11,21 @@ eigendecompose(Matrix<T, N, N> &matrix)
   Matrix<V, N, N> eigenvectors = Matrix<V, N, N>::identity();
   Matrix<V, N, N> deviation    = Matrix<V, N, N>::identity();
 
-  while (deviation.norm() > 1e-10)
+  for (int i = 0; i < 500 && deviation.norm() > 1e-8; i++)
   {
     deviation = matrix;
 
-    const Matrix<V, N, N> &q = qr_decompose(matrix);
-    matrix = matrix*q;
+    int k = i%(N - 1) + 1;
+    const T &a = matrix(k - 1, k - 1);
+    const T &b = matrix(k - 1, k    );
+    const T &c = matrix(k    , k    );
+    const V &d = (a - c)/2;
+    const Matrix<V, N, N> &shift = (c - sign(d)*b*b/(std::abs(d) + std::hypot(d, b)))*Matrix<V, N, N>::identity();
 
+    Matrix<V, N, N> r = matrix - shift;
+    const Matrix<V, N, N> &q = qr_decompose(r);
+
+    matrix = r*q + shift;
     eigenvectors = eigenvectors*q;
 
     deviation -= matrix;
