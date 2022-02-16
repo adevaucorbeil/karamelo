bb85a2825cadfea2b048e08322b3a1234fc18cca
Edward Buckland <edwardnbuckland@gmail.com>
2022-02-17 00:15:54 +1100

    Expression is a big pain

--- src/mathematics/expression/deprecated/expression1.h
+++ src/mathematics/expression/deprecated/expression1.h
@@ -1,18 +1,30 @@
 #pragma once
 
-#include <expression_calculator.h>
+#include <ostream>
+
+#include <Kokkos_Macros.hpp>
 
 class Solid;
 class Grid;
 
+template<typename T>
 class Expression
 {
   bool constant;
   bool only_time_dependent;
 
-  double value_cache;
+  virtual KOKKOS_INLINE_FUNCTION void
+  updateValueCache(const Solid *solid, int ip, const Grid *grid, int in) const = 0;
 
-  /*
+  virtual void
+  print(std::ostream &stream) const = 0;
+
+  virtual constexpr int
+  precedence() const = 0;
+
+protected:
+  T value_cache;
+
   KOKKOS_INLINE_FUNCTION
   Expression():
     Expression(true, true)
@@ -24,19 +36,22 @@ class Expression
   {}
   template<typename ...U>
   KOKKOS_INLINE_FUNCTION
-  Expression(const ExpressionBase &child, U &&...children):
+  Expression(const Expression<T> &child, U &&...children):
     Expression(children...)
   {
     constant            &= child.constant;
     only_time_dependent &= child.only_time_dependent;
   }
-  */
+
+  virtual KOKKOS_INLINE_FUNCTION
+  ~Expression()
+  {}
 
   KOKKOS_INLINE_FUNCTION const T &
   getValue(const Solid *solid, int ip, const Grid *grid, int in, bool time_changed) const
   {
-    if (!constant && (!only_time_dependent || time_changed)
-      value_cache = ExpressionCalculator::calculate()
+    if (constant || only_time_dependent && !time_changed)
+      updateValueCache(solid, ip, grid, in);
 
     return value_cache;
   }
@@ -52,5 +67,14 @@ public:
   operator()(const Grid &grid, int in, bool time_changed) const
   {
     return getValue(nullptr, 0, &grid, in, time_changed);
+  }
+
+  virtual KOKKOS_INLINE_FUNCTION Expression<T>
+  timeDerivative() const = 0;
+
+  friend std::ostream &
+  operator<<(std::ostream &steam, const Expression<T> &expression)
+  {
+    expression.print(stream);
   }
 };
