947084f9f7efef2c2d22f99cf19dc55af44dfe53
Alban de Vaucorbeil <alban.devaucorbeil@deakin.edu.au>
2022-05-03 14:09:02 +1000

    Update fix_body_force

--- src/style/fix/fix_body_force.cpp
+++ src/style/fix/fix_body_force.cpp
@@ -11,21 +11,20 @@
  *
  * ----------------------------------------------------------------------- */
 
-
 #include <fix_body_force.h>
-#include <input.h>
-#include <group.h>
 #include <domain.h>
+#include <error.h>
+#include <expression_operation.h>
+#include <grid.h>
+#include <group.h>
 #include <input.h>
 #include <universe.h>
-#include <grid.h>
-#include <error.h>
 
 using namespace std;
 using namespace FixConst;
 
 
-FixBodyforce::FixBodyforce(MPM *mpm, vector<string> args):
+FixBodyForce::FixBodyForce(MPM *mpm, vector<string> args):
   Fix(mpm, args, POST_PARTICLES_TO_GRID)
 {
   if (args.size() < 3) {
@@ -45,109 +44,112 @@ FixBodyforce::FixBodyforce(MPM *mpm, vector<string> args):
     return;
   }
 
-  if (domain->dimension == 3 && args.size()<6) {
-    error->all(FLERR,"Error: too few arguments for fix_body_force: requires at least 6 arguments. " + to_string(args.size()) + " received.\n");
-  } else if (domain->dimension == 2 && args.size()<5) {
-    error->all(FLERR,"Error: too few arguments for fix_body_force: requires at least 5 arguments. " + to_string(args.size()) + " received.\n");
-  } else if (domain->dimension == 1 && args.size()<4) {
-    error->all(FLERR,"Error: too few arguments for fix_body_force: requires at least 4 arguments. " + to_string(args.size()) + " received.\n");
+  if (args.size() < Nargs.find(domain->dimension)->second) {
+    error->all(FLERR, "Error: too few arguments for fix_body_force.\n" +
+                          usage.find(domain->dimension)->second);
   }
 
-  if (group->pon[igroup].compare("nodes") !=0 && group->pon[igroup].compare("all") !=0) {
-    error->all(FLERR,"fix_body_force needs to be given a group of nodes" + group->pon[igroup] + ", " + args[2] + " is a group of " + group->pon[igroup] + ".\n");
+  if (group->pon[igroup].compare("nodes") !=0  && group->pon[igroup].compare("all") !=0) {
+    error->one(FLERR, "fix_body_force needs to be given a group of nodes" + group->pon[igroup] + ", " + args[2] + " is a group of "+ group->pon[igroup] + ".\n");
   }
   if (universe->me == 0) {
-    cout << "Creating new fix FixBodyforce with ID: " << args[0] << endl;
+    cout << "Creating new fix FixBodyForce with ID: " << args[0] << endl;
   }
   id = args[0];
 
   xset = yset = zset = false;
 
   if (args[3] != "NULL") {
-    xvalue = input->parsev(args[3]);
     xset = true;
+    input->parsev(args[3]);
+
+    fb[0] = &input->expressions[args[3]];
   }
 
-  if (domain->dimension >= 2) {
-    if (args[4] != "NULL") {
-      yvalue = input->parsev(args[4]);
+  if (domain->dimension >= 2 && args[4] != "NULL")
+  {
       yset = true;
-    }
+      input->parsev(args[4]);
+
+      fb[1] = &input->expressions[args[4]];
+  } else {
+    fb[1] = nullptr;
   }
 
-  if (domain->dimension == 3) {
-    if (args[5] != "NULL") {
-      zvalue = input->parsev(args[5]);
+  if (domain->dimension == 3 && args[5] != "NULL")
+    {
+      input->parsev(args[5]);
       zset = true;
+
+      fb[2] = &input->expressions[args[5]];
     }
+  else {
+    fb[2] = nullptr;
   }
 }
 
-void FixBodyforce::prepare()
+void FixBodyForce::prepare()
 {
-  xvalue.result(mpm);
-  yvalue.result(mpm);
-  zvalue.result(mpm);
-
   ftot = Vector3d();
 }
 
-void FixBodyforce::reduce()
+void FixBodyForce::reduce()
 {
   Vector3d ftot_reduced;
   MPI_Allreduce(ftot.elements, ftot_reduced.elements, 3, MPI_DOUBLE, MPI_SUM, universe->uworld);
-
-  if (xset) (*input->vars)[id + "_x"] = Var(id + "_x", ftot_reduced[0]);
-  if (yset) (*input->vars)[id + "_y"] = Var(id + "_y", ftot_reduced[1]);
-  if (zset) (*input->vars)[id + "_z"] = Var(id + "_z", ftot_reduced[2]);
-  // cout << "ftot = [" << ftot[0] << ", " << ftot[1] << ", " << ftot[2] << "], mass = " << mtot << "\n"; 
 }
 
-void FixBodyforce::post_particles_to_grid(Grid &grid)
+void FixBodyForce::post_particles_to_grid(Grid &grid)
 {
-  for (int in = 0; in < grid.nnodes_local + grid.nnodes_ghost; in++)
-  {
-    if (!grid.mass[in] || !(grid.mask[in] & groupbit))
-      continue;
+  
+  // Go through all the nodes in the group and set fb to the right value:
 
-    (*input->vars)["x0"] = Var("x0", grid.x0[in][0]);
-    (*input->vars)["y0"] = Var("y0", grid.x0[in][1]);
-    (*input->vars)["z0"] = Var("z0", grid.x0[in][2]);
+  for (int i = 0; i < 3; i++)
+    if (fb[i])
+      fb[i]->evaluate(grid);
 
-    Vector3d f;
-    if (xset) f[0] = xvalue.result(mpm, true);
-    if (yset) f[1] = yvalue.result(mpm, true);
-    if (zset) f[2] = zvalue.result(mpm, true);
+  int groupbit = this->groupbit;
 
-    grid.mb[in] += grid.mass[in]*f;
+  for (int i = 0; i < 3; i++)
+    if (fb[i])
+    {
+      Kokkos::View<double **> fb_i = fb[i]->registers;
 
-    if (in < grid.nnodes_local)
-        ftot += f;
-  }
+      Kokkos::parallel_reduce("FixBodyForce::post_particles_to_grid", grid.nnodes_local + grid.nnodes_ghost,
+                              KOKKOS_LAMBDA(const int &in, double &ftot_i)
+      {
+	if (!grid.mass[in] || !(grid.mask[in] & groupbit))
+          return;
+
+	grid.mb[in][i] += grid.mass[in]*fb_i(0, in);
+
+        ftot_i += fb_i(0, in);
+      }, ftot[i]);
+    }
 }
 
-void FixBodyforce::write_restart(ofstream *of) {
+void FixBodyForce::write_restart(ofstream *of) {
   of->write(reinterpret_cast<const char *>(&xset), sizeof(bool));
   of->write(reinterpret_cast<const char *>(&yset), sizeof(bool));
   of->write(reinterpret_cast<const char *>(&zset), sizeof(bool));
 
-  if (xset)
-    xvalue.write_to_restart(of);
-  if (yset)
-    yvalue.write_to_restart(of);
-  if (zset)
-    zvalue.write_to_restart(of);
+  // if (xset)
+  //   xvalue.write_to_restart(of);
+  // if (yset)
+  //   yvalue.write_to_restart(of);
+  // if (zset)
+  //   zvalue.write_to_restart(of);
 }
 
-void FixBodyforce::read_restart(ifstream *ifr) {
+void FixBodyForce::read_restart(ifstream *ifr) {
   ifr->read(reinterpret_cast<char *>(&xset), sizeof(bool));
   ifr->read(reinterpret_cast<char *>(&yset), sizeof(bool));
   ifr->read(reinterpret_cast<char *>(&zset), sizeof(bool));
 
-  if (xset)
-    xvalue.read_from_restart(ifr);
-  if (yset)
-    yvalue.read_from_restart(ifr);
-  if (zset)
-    zvalue.read_from_restart(ifr);
+  // if (xset)
+  //   xvalue.read_from_restart(ifr);
+  // if (yset)
+  //   yvalue.read_from_restart(ifr);
+  // if (zset)
+  //   zvalue.read_from_restart(ifr);
 }
--- src/style/fix/fix_body_force.h
+++ src/style/fix/fix_body_force.h
@@ -13,7 +13,7 @@
 
 #ifdef FIX_CLASS
 
-FixStyle(body_force,FixBodyforce)
+FixStyle(body_force,FixBodyForce)
 
 #else
 
@@ -21,8 +21,9 @@ FixStyle(body_force,FixBodyforce)
 #define MPM_FIX_BODY_FORCE_H
 
 #include <fix.h>
-#include <var.h>
 #include <matrix.h>
+
+class Expression;
 
 /*! \ingroup fix fixbodyforce fix_body_force
 
@@ -58,9 +59,9 @@ fz
 
 \section Class Class description
 */
-class FixBodyforce : public Fix {
+class FixBodyForce : public Fix {
  public:
-  FixBodyforce(MPM *, vector<string>);
+  FixBodyForce(MPM *, vector<string>);
   
   void prepare();
   void reduce();
@@ -70,10 +71,18 @@ class FixBodyforce : public Fix {
   void write_restart(ofstream *);
   void read_restart(ifstream *);
 
+  
 private:
-  Var xvalue, yvalue, zvalue;    // Set force in x, y, and z directions.
-  bool xset, yset, zset;               // Does the fix set the x, y, and z forces of the group?
+  const map<int, string> usage = {
+      {1, "Usage: fix(fix-ID, body_force, group, fbx)\n"},
+      {2, "Usage: fix(fix-ID, body_force, group, fbx, fby)\n"},
+      {3, "Usage: fix(fix-ID, body_force, group, fbx, fby, fbz)\n"}};
+  const map<int, int> Nargs = {{1, 4}, {2, 5}, {3, 6}};
+
+  bool xset, yset, zset;                             //< Does the fix set the x, y, and z velocities of the group?
   Vector3d ftot;
+
+  Expression *fb[3];
 };
 
 #endif
