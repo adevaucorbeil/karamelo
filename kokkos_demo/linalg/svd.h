7c641910069d2e99c0a0eb412f6dd290a55b30fa
Edward Buckland <ebuc0009@student.monash.edu>
2022-07-12 00:32:21 +1000

    float for vectors and matrices

--- kokkos_demo/linalg/svd.h
+++ kokkos_demo/linalg/svd.h
@@ -9,21 +9,21 @@ void svd(const Matrix<T, 2, 2> &A,
   Matrix<W, 2, 2> &sig,
   Matrix<W, 2, 2> &V)
 {
-  double x = A(0, 0) + A(1, 1), y = A(1, 0) - A(0, 1);
-  double scale = 1/sqrt(x*x + y*y);
-  double c = x*scale;
-  double s = y*scale;
+  W x = A(0, 0) + A(1, 1), y = A(1, 0) - A(0, 1);
+  W scale = 1/sqrt(x*x + y*y);
+  W c = x*scale;
+  W s = y*scale;
   Matrix<W, 2, 2> R(c, -s, s, c);
   Matrix<W, 2, 2> S = R.transpose()*A;
-  double s1 = 0, s2 = 0;
+  W s1 = 0, s2 = 0;
   if (abs(S(0, 1)) < 1e-5) {
     c = 1;
     s = 0;
   }
   else {
-   double tao = 0.5*(S(0, 0) - S(1, 1));
-   double w = sqrt(tao*tao + S(0, 1)* S(0, 1));
-   double t = 0;
+   W tao = 0.5*(S(0, 0) - S(1, 1));
+   W w = sqrt(tao*tao + S(0, 1)* S(0, 1));
+   W t = 0;
    if (tao > 0)
      t = S(0, 1)/(tao + w);
    else
@@ -35,7 +35,7 @@ void svd(const Matrix<T, 2, 2> &A,
   }
        
   if (s1 < s2) {
-    double tmp = s1;
+    W tmp = s1;
     s1 = s2;
     s2 = tmp;
     V = Matrix<W, 2, 2>(-s, c, -c, -s);
